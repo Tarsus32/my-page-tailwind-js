2af256c659c40a1df78fd00e0cbe64c81c743074
Milan <milan.gmitro32@gmail.com>
2021-05-20 14:23:15 +0200

    zmena ciest obrazkov

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    purge: [],
+    purge: {
+    enabled: true, 
+     content: [
+        './public/*.html',
+        './src/*/**.js',
+      ],
+    }, 
     darkMode: false, 
     theme: {
       extend: {
@@ -7,20 +13,20 @@ module.exports = {
           body: ['Poppins']
         },
       backgroundImage: theme => ({
-        'my-profile-foto': "url('/src/img/profile_photo.jpg')",
-        'home-title': "url('/src/img/titleimg2.jpg')",
-        'about-title': "url('/src/img/titleimg3.jpg')",
-        'about-more': "url('/src/img/titleimg4.jpg')",
-        'contact-title': "url('/src/img/titleimgcontact.jpg')",
-        'road-back': "url('/src/img/about/learn.jpg')",
-        'road-left': "url('/src/img/about/road1.jpg')",
-        'road-right': "url('/src/img/about/road2.jpg')",
-        'hobby-back': "url('/src/img/about/workout.jpeg')",
-        'hobby-left': "url('/src/img/about/hobby1.jpg')",
-        'hobby-right': "url('/src/img/about/hobby2.jpg')",
-        'future-back': "url('/src/img/about/future-back.jpg')",
-        'future-left': "url('/src/img/about/future1.jpg')",
-        'future-right': "url('/src/img/about/future2.jpg')",
+        'my-profile-foto': "url('../public/img/profile_photo.jpg')",
+        'home-title': "url('../public/img/titleimg2.jpg')",
+        'about-title': "url('../public/img/titleimg3.jpg')",
+        'about-more': "url('../public/img/titleimg4.jpg')",
+        'contact-title': "url('../public/img/titleimgcontact.jpg')",
+        'road-back': "url('../public/img/about/learn.jpg')",
+        'road-left': "url('../public/img/about/road1.jpg')",
+        'road-right': "url('../public/img/about/road2.jpg')",
+        'hobby-back': "url('../public/img/about/workout.jpeg')",
+        'hobby-left': "url('../public/img/about/hobby1.jpg')",
+        'hobby-right': "url('../public/img/about/hobby2.jpg')",
+        'future-back': "url('../public/img/about/future-back.jpg')",
+        'future-left': "url('../public/img/about/future1.jpg')",
+        'future-right': "url('../public/img/about/future2.jpg')",
        }),
     zIndex: {
         '-1': '-1',
